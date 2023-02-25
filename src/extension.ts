afb04e976b8a78ce183bc56bc74e4ae7d7083214
Steven Lovegrove <stevenlovegrove@gmail.com>
2023-02-25 02:50:31 -0800

    Persist history

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ type Target = string;
 type ArgList = string;
 
 const empty : string = "[empty]";
-let history = new Map<Target, ArgList[]>();
+let history = {};
 let currentTarget : string = "";
 let currentArgs : string = "";
 let currentDoDebug : boolean = false;
@@ -17,16 +17,14 @@ function targetArgsCustomized(commandline : string | undefined) {
 	if(typeof commandline === 'undefined') {return;}
 
 	currentArgs = commandline;
-	let hist = history.get(currentTarget);
+	let hist = history[currentTarget];
 	if(!hist) {hist = [];}
 
 	if(commandline !== "") {
 		hist.unshift(commandline);
 	}
 
-	history.set(currentTarget,
-		hist.filter((item,index) => { return hist?.indexOf(item) === index; })
-	);
+	history[currentTarget] =  hist.filter((item,index) => { return hist?.indexOf(item) === index; });
 
 	vscode.window.showInformationMessage("Launching: '" + currentTarget + " " + currentArgs + "'");
 	let thisLaunch = {
@@ -42,8 +40,7 @@ function targetArgsCustomized(commandline : string | undefined) {
 	}
 
 	// Save list to persist
-	let json = Array.from(history.entries()).reduce((o, [key, value]) => { o[key] = value; return o; }, {});
-	vscode.workspace.getConfiguration().update("launcharoo.history", json, vscode.ConfigurationTarget.Global);
+	vscode.workspace.getConfiguration().update("launcharoo.history", history, vscode.ConfigurationTarget.Global);
 }
 
 // precondition: currentTarget has been set
@@ -62,7 +59,7 @@ function targetLaunched(target : unknown) {
 	if(typeof target !== 'string') {return;}
 
 	currentTarget = target;
-	let targetHistory = history.get(target);
+	let targetHistory = history[target];
 	if(targetHistory && targetHistory.length > 0) {
 		vscode.window.showQuickPick([empty].concat(targetHistory),
 		{
@@ -78,10 +75,7 @@ function targetLaunched(target : unknown) {
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "launcharoo" is now active!');
+	history = vscode.workspace.getConfiguration().get("launcharoo.history", {});
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
