b9cf14f82aa9b4a7a491848c6b8b007f2092f015
Steven Lovegrove <stevenlovegrove@gmail.com>
2023-02-26 00:28:40 -0800

    Add a few features

--- src/extension.ts
+++ src/extension.ts
@@ -6,11 +6,25 @@ import * as vscode from 'vscode';
 type Target = string;
 type ArgList = string;
 
-const empty : string = "[empty]";
+const empty : string = "[blank]";
 let history = {};
 let currentTarget : string = "";
 let currentArgs : string = "";
 let currentDoDebug : boolean = false;
+
+function saveConfig() : void {
+	vscode.workspace.getConfiguration().update("launcharoo.history", history, vscode.ConfigurationTarget.Global);
+}
+
+function maxHistoryPerTarget() : number {
+	let num = vscode.workspace.getConfiguration().get("launcharoo.maxHistoryPerTarget", {});
+	return typeof num === 'number' ? num : 100;
+}
+
+function editArgumentsBeforeLaunch() : boolean {
+	let v = vscode.workspace.getConfiguration().get("launcharoo.editArgumentsBeforeLaunch", {});
+	return typeof v === 'boolean' ? v : true;
+}
 
 // precondition: currentTarget has been set
 function targetArgsCustomized(commandline : string | undefined) {
@@ -22,9 +36,10 @@ function targetArgsCustomized(commandline : string | undefined) {
 
 	if(commandline !== "") {
 		hist.unshift(commandline);
+		hist = hist.filter((item,index) => { return hist?.indexOf(item) === index; });
+		hist = hist.slice(0, Math.min(hist.length, maxHistoryPerTarget()) );
+		history[currentTarget] = hist;
 	}
-
-	history[currentTarget] =  hist.filter((item,index) => { return hist?.indexOf(item) === index; });
 
 	vscode.window.showInformationMessage("Launching: '" + currentTarget + " " + currentArgs + "'");
 	let thisLaunch = {
@@ -39,57 +54,134 @@ function targetArgsCustomized(commandline : string | undefined) {
 		vscode.commands.executeCommand("cmake.launchTarget");
 	}
 
-	// Save list to persist
-	vscode.workspace.getConfiguration().update("launcharoo.history", history, vscode.ConfigurationTarget.Global);
+	saveConfig();
 }
 
 // precondition: currentTarget has been set
-function targetArgsSelected(commandline : string | undefined) {
+function targetArgsSelected(selected : any) {
+	let commandline = selected?.label;
 	if(typeof commandline === 'undefined') {return;}
 
-	let cmd : string = (commandline === empty) ? "" : commandline;
-	vscode.window.showInputBox({
-		title: "Customize Command Line",
-		value: cmd,
-		prompt: "Enter Arguments for '" + currentTarget + "'"
-	}).then(targetArgsCustomized);
+	if(editArgumentsBeforeLaunch() || commandline === empty) {
+		let cmd : string = (commandline === empty) ? "" : commandline;
+		vscode.window.showInputBox({
+			title: "Modify the command line, or just hit enter!",
+			value: cmd,
+			prompt: "Enter Arguments for '" + currentTarget + "'"
+		}).then(targetArgsCustomized);
+	}else{
+		targetArgsCustomized(commandline);
+	}
+}
+
+function targetClear(target : unknown) {
+	if(typeof target !== 'string') {return;}
+	currentTarget = target;
+	history[currentTarget] = [];
+	saveConfig();
 }
 
 function targetLaunched(target : unknown) {
 	if(typeof target !== 'string') {return;}
 
-	currentTarget = target;
-	let targetHistory = history[target];
-	if(targetHistory && targetHistory.length > 0) {
-		vscode.window.showQuickPick([empty].concat(targetHistory),
-		{
-			title: "Select existing Command Line to Customize",
-			placeHolder: "Arguments for '" + currentTarget + "'"
-		}
-		).then(targetArgsSelected);
-	}else{
-		targetArgsSelected("");
+	if( target === '') {
+		pickAndLaunch;
 	}
+
+	currentTarget = target;
+
+	// arg history for selected target
+	let targetHistory = history[target];
+
+	// arg history for other targets
+	let otherHistory : string[] = [];
+	Object.keys(history).forEach(key => {
+		let otherTargetHistory = history[key];
+		otherTargetHistory.forEach((arglist:string) => {
+			if( (!targetHistory || targetHistory.indexOf(arglist) < 0) && otherHistory.indexOf(arglist) < 0) {
+				otherHistory.push(arglist);
+			}
+		});
+	});
+
+	if(targetHistory?.length === 0 && otherHistory?.length === 0) {
+		targetArgsSelected({label:""});
+	}else{
+		let items : vscode.QuickPickItem[] = [];
+		items.push({kind: vscode.QuickPickItemKind.Separator, label:"Start from scratch"});
+		items.push({label: empty});
+
+		if(targetHistory?.length > 0) {
+			items.push({kind: vscode.QuickPickItemKind.Separator, label:"Recent"});
+			targetHistory.forEach((arglist:string) => {
+				items.push({label: arglist});
+			});
+		}
+
+		if(otherHistory?.length > 0) {
+			items.push({kind: vscode.QuickPickItemKind.Separator, label:"From other targets"});
+			otherHistory.forEach((other:string) => {
+				items.push({label: other});
+			});
+		}
+
+		vscode.window.showQuickPick(items, {
+			title: "Select command line arguments to customize",
+			placeHolder: "Arguments for '" + currentTarget + "'"
+		} ).then(targetArgsSelected);
+
+	}
+}
+
+async function pickAndLaunch() {
+	await vscode.commands.executeCommand('cmake.selectLaunchTarget');
+	vscode.commands.executeCommand('cmake.launchTargetPath').then(targetLaunched);
+}
+
+function common() {
+	history = vscode.workspace.getConfiguration().get("launcharoo.history", {});
 }
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	history = vscode.workspace.getConfiguration().get("launcharoo.history", {});
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable1 = vscode.commands.registerCommand('launcharoo.launch', () => {
-		currentDoDebug = false;
-		vscode.commands.executeCommand('cmake.launchTargetPath').then(targetLaunched);
-	});
-	let disposable2 = vscode.commands.registerCommand('launcharoo.debug', () => {
-		currentDoDebug = true;
-		vscode.commands.executeCommand('cmake.launchTargetPath').then(targetLaunched);
-	});
-	context.subscriptions.push(disposable1);
-	context.subscriptions.push(disposable2);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('launcharoo.pickAndLaunch', () => {
+			common();
+			currentDoDebug = false;
+			pickAndLaunch();
+		}),
+		vscode.commands.registerCommand('launcharoo.launch', () => {
+			common();
+			currentDoDebug = false;
+			vscode.commands.executeCommand('cmake.launchTargetPath').then(targetLaunched);
+		}),
+		vscode.commands.registerCommand('launcharoo.debug', () => {
+			common();
+			currentDoDebug = true;
+			vscode.commands.executeCommand('cmake.launchTargetPath').then(targetLaunched);
+		}),
+		vscode.commands.registerCommand('launcharoo.again', () => {
+			common();
+			targetArgsCustomized(currentArgs);
+		}),
+		vscode.commands.registerCommand('launcharoo.clear', () => {
+			common();
+			currentDoDebug = true;
+			vscode.commands.executeCommand('cmake.launchTargetPath').then(targetClear);
+		}),
+		vscode.commands.registerCommand('launcharoo.settings', () => {
+			common();
+			currentDoDebug = true;
+			vscode.commands.executeCommand( 'workbench.action.openSettings', 'launcharoo' );
+		})
+
+
+
+	);
 }
 
 // This method is called when your extension is deactivated
